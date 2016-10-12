fada18b121cf724f97a1a2a1ea1ba54a020fdaf3
pianne pong <wonglianne@yahoo.com>
2016-10-11 18:00:40 -0700

    game

---
+++ main.js
@@ -0,0 +1,42 @@
+// We create our only state
+var mainState = {
+    // Here we add all the functions we need for our state
+    // For this project we will just have 3
+    preload: function () {
+        // This function will be executed at the beginning
+        // That's where we load the game's assets
+        // Load the image
+        game.load.image("napoleon","napoleon.jpg");
+    }
+    , create: function () {
+        // This function is called after the 'preload' function 
+        // Here we set up the game, display sprites, etc.
+        this.player = game.add.sprite(0,100,"napoleon");
+        this.player.scale.setTo(.35,.35);
+        this.physics.arcade.enable(this.player);
+        this.player.body.gravity.y=300;
+        this.player.body.collideWorldBounds = true;
+        this.keyboard=game.input.keyboard.createCursorKeys();
+        
+    }
+    , update: function () {
+        this.player.body.velocity.x=0;
+        if (this.keyboard.right.isDown){
+            this.player.body.velocity.x=300;
+            }else if(this.keyboard.left.isDown){
+                this.player.body.velocity.x=-300;
+            }
+        if (this.keyboard.up.isDown) {
+            this.player.body.velocity.y=-300;
+        }else if(this.keyboard.down.isDown){
+            this.player.body.velocity.y=300;
+        }
+        
+        // This contains Game Logic 
+    }
+};
+// We initialize Phaser
+var game = new Phaser.Game(800, 800, Phaser.AUTO, '');
+// And we tell Phaser to add and start our 'main' state
+game.state.add('main', mainState);
+game.state.start('main');
